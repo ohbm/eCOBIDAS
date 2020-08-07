f4012bccaa13a43b8f81235065e14262f91f5144
Remi Gau <remi_gau@hotmail.com>
2020-08-07 10:04:18 +0200

    change schema name to neurovault

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,6 +1,6 @@
 module.exports {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/cobidas/master/protocols/cobidas_schema',
+  githubSrc: 'https://raw.githubusercontent.com/Remi-Gau/cobidas/master/protocols/neurovault_schema',
   contentSrc: 'https://raw.githubusercontent.com/Remi-Gau/cobidas/master/protocols/README-en.md'
   banner: 'This protocol is under construction.',
   startButton: 'Start',
