be746490a152713111a2d7d88fccafcc2da8f338
Remi Gau <remi_gau@hotmail.com>
2020-08-08 07:17:11 +0200

    update landing page

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   /* eslint-disable */
   githubSrc: 'https://raw.githubusercontent.com/ohbm/cobidas/master/protocols/neurovault/neurovault_schema',
-  contentSrc: 'https://raw.githubusercontent.com/ohbm/cobidas/master/protocols/README-en.md',
   banner: 'This protocol is under construction.',
   startButton: 'Start',
   assetsPublicPath: '/cobidas/',
