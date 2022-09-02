12be522e522226ed590d5e9eec1e75406b1dc42f
Remi Gau <remi_gau@hotmail.com>
2022-09-02 12:51:56 +0200

    Update config.js

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ohbm/cobidas_schema/master/schemas/neurovault/protocols/neurovault_schema.jsonld',
-  banner: 'This protocol is under construction.',
+  githubSrc: 'https://raw.githubusercontent.com/ohbm/cobidas_schema/master/schemas/cobidas/protocols/cobidas_schema.jsonld',
+  banner: 'This checklist is under construction.',
   startButton: 'Start',
   assetsPublicPath: '/eCOBIDAS/',
   backendServer: 'null'
